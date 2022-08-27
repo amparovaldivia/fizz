0f51f21a8253a9064e4188c362dba39b4938bf5c
unknown <amparo.valdivia@gmail.com>
2022-08-27 03:21:31 -0400

    first commit

---
+++ fizz.js
@@ -0,0 +1,15 @@
+for(var x=0;x<100;x++){
+    if(x%3==0){
+        if(x%5==0){
+            console.log("FizzBuzz")
+        }
+        else {
+        console.log("Fizz");
+        }
+    }else if(x%5==0){
+        console.log("Buzz");
+    
+    }else {
+        console.log(x)
+    }
+}
